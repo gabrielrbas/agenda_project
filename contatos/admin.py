4f6e20ba7a0901ebeb210b174fd9f906e4de30bf
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-05-24 03:03:58 -0300

    updated admin, models, migrations

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone',
+                    'email', 'data_criacao', 'categoria')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    search_fields = ('id', 'nome', 'sobrenome', 'telefone')
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -12,9 +12,9 @@ class Categoria(models.Model):
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
-    sobrenome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
     telefone = models.CharField(max_length=20)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100, blank=True)
     data_criacao = models.DateTimeField(timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=DO_NOTHING)
