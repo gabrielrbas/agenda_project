846b678bc776ae4148aae546d4070f003b60a0e5
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-05-24 03:10:32 -0300

    updated models

--- contatos/models.py
+++ contatos/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models.deletion import DO_NOTHING
 from django.utils import timezone
 
 
@@ -15,9 +14,9 @@ class Contato(models.Model):
     sobrenome = models.CharField(max_length=255, blank=True)
     telefone = models.CharField(max_length=20)
     email = models.EmailField(max_length=100, blank=True)
-    data_criacao = models.DateTimeField(timezone.now)
+    data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
-    categoria = models.ForeignKey(Categoria, on_delete=DO_NOTHING)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.nome
