221b20fab8751d740b5607c61da46c71b3c53cc3
Gabriel Ribas <gabrielr.bas@gmail.com>
2021-05-24 02:53:40 -0300

    added models, migrations

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models.deletion import DO_NOTHING
+from django.utils import timezone
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    data_criacao = models.DateTimeField(timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
